2f0bae43b02c25203fd5ab5efced923e398920ac
gdsglgf <wzguofulgz@126.com>
2015-10-13 16:28:39 +0800

    Add insetion and merge sort algotithm java implementation

---
+++ sorting/CommUtil.java
@@ -0,0 +1,14 @@
+public class CommUtil {
+	public static void swap(int[] data, int i, int j) {
+		int temp = data[i];
+		data[i] = data[j];
+		data[j] = temp;
+	}
+
+	public static int[] arrayCopy(int[] data) {
+		int[] dest = new int[data.length];
+		System.arraycopy(data, 0, dest, 0, data.length);
+
+		return dest;
+	}
+}	
