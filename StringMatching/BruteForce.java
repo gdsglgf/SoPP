a08b95dd2baae7ef55921506ab0c5e4a5f79e9e8
gdsglgf <wzguofulgz@126.com>
2015-11-14 14:55:20 +0800

    Add just one loop implementation

--- StringMatching/BruteForce.java
+++ StringMatching/BruteForce.java
@@ -27,6 +27,31 @@ public class BruteForce {
         return -1;
     }
 
+    public static int match2(String text, String pattern) {
+        if (text == null || pattern == null || pattern.equals("")
+            || text.length() < pattern.length()) {
+            return -1;
+        }
+
+        int textLength = text.length();
+        int patternLength = pattern.length();
+        int p = 0;      // 模式串下标
+        int t = 0;      // 文本串下标
+        while (p < patternLength && t < textLength) {
+            if (text.charAt(t) == pattern.charAt(p)) {
+                p++;
+                t++;
+            } else {
+                t = t - p + 1;      // 计算文本串比较的起始位置, t - p为上一次比较的起始位置
+                p = 0;
+            }
+        }
+        if (p >= patternLength) {
+            return t - p;
+        }
+        return -1;
+    }
+
     public static void main (String[] args) throws FileNotFoundException {
         Scanner sc = new Scanner(new File(args[0]));
         String line;
@@ -34,7 +59,7 @@ public class BruteForce {
             line = sc.nextLine();
             line = line.trim();
             String[] elements = line.split(" \\| ");
-            System.out.println(match(elements[0], elements[1]));
+            System.out.printf("%d = %d\n", match(elements[0], elements[1]), match2(elements[0], elements[1]));
         }
         sc.close();
     }
