0904379a7fc1ee772ab395998d87cbf2325bf2ff
gdsglgf <wzguofulgz@126.com>
2016-05-06 11:07:11 +0800

    Add bubblesort

---
+++ Sorting/BubbleSort.java
@@ -0,0 +1,22 @@
+import java.util.Arrays;
+
+public class BubbleSort {
+    public static void bubbleSort(int[] data) {
+        int last = data.length - 1;
+        for (int i = 0; i < last; i++) {
+            for (int j = last; j > i; j--) {
+                if (data[j] < data[j - 1]) {
+                    CommUtil.swap(data, j, j - 1);
+                }
+            }
+            System.out.println(Arrays.toString(data));
+        }
+    }
+
+    public static void main(String[] args) {
+        int[] data = {9, 8, 7, 6, 5, 4, 3, 2, 1, 0};
+        System.out.println(Arrays.toString(data));
+        bubbleSort(data);
+        System.out.println(Arrays.toString(data));
+    }
+}
